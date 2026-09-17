fb63c19bba01043a29751567d7322da659dd1a9b
SaudG5 <ssssgg9000.com>
2026-09-17 16:34:28 +0300

    print test in python

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+Language = "Python"
+Version = "3"
+Pi_approx = 3.14
+Computation_valid = (2+2 == 4)
+
+
+print(f"Language: {Language}")
+print(f"Version: {Version}")
+print(f"Pi approx: {Pi_approx}")
+print(f"Computation valid: {Computation_valid}")
+print()
