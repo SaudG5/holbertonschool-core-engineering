03f9a1a133a09295bc678edacb5e5837de282d46
SaudG5 <ssssgg9000.com>
2026-09-17 17:20:11 +0300

    Edit file

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 Language = "Python"
-Version = "3"
+Version = 3
 Pi_approx = 3.14
 Computation_valid = (2+2 == 4)
 
@@ -9,4 +9,3 @@ print(f"Language: {Language}")
 print(f"Version: {Version}")
 print(f"Pi approx: {Pi_approx}")
 print(f"Computation valid: {Computation_valid}")
-print()
