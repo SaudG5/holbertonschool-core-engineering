e8fb8d1d6cd58b07b8f796516031fba3a5e49f1a
SaudG5 <ssssgg9000.com>
2026-09-18 20:21:37 +0300

    task3

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+result = ""
 for i in range(ord('a'), ord('z') + 1):
     if (i == ord('q') or i == ord('e')):
         continue
-    print(chr(i), end="")
+    result += chr(i)
+print(result)
