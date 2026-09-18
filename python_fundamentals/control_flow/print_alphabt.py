86da7caea1f798dba362e59ca519de2a94e786cd
SaudG5 <your.actual.email@example.com>
2026-09-18 20:00:10 +0200

    edit

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -4,4 +4,4 @@ for i in range(ord('a'), ord('z') + 1):
     if (i == ord('q') or i == ord('e')):
         continue
     result += chr(i)
-print("{}".format(result))
+print("{}".format(result), end="")
