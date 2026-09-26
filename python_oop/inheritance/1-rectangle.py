4068b4e32aeaf4f3dd446d2de307d3e203189ec6
SaudG5 <ssssgg9000.com>
2026-09-26 18:35:57 +0300

    Geometry OOP tasks

---
+++ python_oop/inheritance/1-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Defines a Rectangle class that inherits from BaseGeometry."""
+BaseGeometry = __import__('base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle."""
+
+    def __init__(self, width, height):
+        """Initialize the rectangle with validated width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
